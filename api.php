804deafca44452db9de65aa144bf8e29f9aceb3e
Jamie W <james.r.watson@att.net>
2013-10-27 00:22:53 -0500

    The general api file I call in my scripts to authenticate
    The general api file I call in my scripts to authenticate

---
+++ api.php
@@ -0,0 +1,8 @@
+<?php
+require_once('softlayer-api-php-client-master/SoftLayer/SoapClient.class.php');
+
+$apiUsername = '<your SL Username>';
+$apiKey = '<your API Key>';
+
+date_default_timezone_set('America/Chicago');
+?>
